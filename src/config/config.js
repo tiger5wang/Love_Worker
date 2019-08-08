259b1b08d48e049d506477a38d70a942263e21ce
wuyongcong <1922878025@qq.com>
2019-08-08 23:29:01 +0800

    ip

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ let isRealease = false;
 
 
 export const UrlConfig = {
-  base_url: isRealease ? 'http://127.0.0.1:9000': 'http://127.0.0.1:9000',
+  base_url: isRealease ? 'http://admin.wuyongcong.com:8002': 'http://admin.wuyongcong.com:8002',
 }
